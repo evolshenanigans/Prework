76820e212f36f8f3b922f29573a70db5a1b5d330
evolshenanigans <gjgutierrez91@gmail.com>
2022-02-28 14:08:39 -0800

    first commit

---
+++ one.py
@@ -0,0 +1,44 @@
+#question 1
+def hello_name(user_name):
+    print("hello "+user_name)
+user_name = input("Enter USERNAME: ")
+hello_name(user_name)
+
+#question 2#
+
+def first_odd():
+    for i in range(1,101):
+        if i % 2 != 0:
+            print(i)
+first_odd()
+
+#question 3#
+def max_num_in_list(a_list):
+    maximum = a_list[0]
+    for i in range(len(a_list)):
+        if a_list[i] > maximum:
+            maximum = a_list[i]
+    return maximum
+a_list = [10, 20, 30, 40, 50]
+print(max_num_in_list(a_list))
+
+#question 4 #
+def is_leap_year(a_year):
+    if(a_year % 400 == 0):
+        return True
+    elif a_year % 100 == 0:
+        return False
+    elif a_year % 4 == 0:
+        return True
+    else:
+        return False 
+a_year = int(input())
+print(is_leap_year(a_year))
+
+#question 5#
+def is_consecutive(a_list):
+    return sorted(a_list) == list(range(min(a_list), max(a_list)+1))
+a_list = [2,3,4,5,6,7]
+print(is_consecutive(a_list))
+a_list = [1,2,4,5]
+print(is_consecutive(a_list))
